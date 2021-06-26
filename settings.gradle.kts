6c65208793fa507aec42ba848537d95158a1ce16
Caleb Cushing <xenoterracide@gmail.com>
2021-06-26 13:46:58 -0400

    reorganize
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,5 +5,8 @@ include(
   ":authn"
 )
 
+project(":app").projectDir = file("module/app")
+project(":authn").projectDir = file("module/authn")
+
 enableFeaturePreview("VERSION_CATALOGS")
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
