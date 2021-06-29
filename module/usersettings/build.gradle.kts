4160ae07d884aa702384f403847a7f81ab723121
Caleb Cushing <xenoterracide@gmail.com>
2021-06-29 18:17:38 -0400

    add usersettings module
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

---
+++ module/usersettings/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+  id("ppm.java-library")
+}
+
+dependencies {
+  implementation(libs.spring.boot.starter.neo4j)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,11 +2,13 @@ rootProject.name = "ppm-backend"
 
 include(
   ":app",
-  ":authn"
+  ":authn",
+  ":usersettings"
 )
 
 project(":app").projectDir = file("module/app")
 project(":authn").projectDir = file("module/authn")
+project(":usersettings").projectDir = file("module/usersettings")
 
 enableFeaturePreview("VERSION_CATALOGS")
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
